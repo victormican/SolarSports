1f74a64969f9946826aacbda9b3356904026c322
victor mican <victormican@gmail.com>
2023-07-24 10:52:25 -0500

    Primera Version Añadidos Boton Logout y Añadido Actualizar Usuario Interfaz

---
+++ app/src/main/java/com/example/solarsports/ActualizarUsuarioActivity.java
@@ -0,0 +1,14 @@
+package com.example.solarsports;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActualizarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_usuario);
+    }
+}
