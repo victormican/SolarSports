84f2dd3e2a46b658c4bb778e604b93b03256c507
victor mican <victormican@gmail.com>
2023-07-25 16:47:51 -0500

    Version 1.0.4 Refactorizacion

--- app/src/main/java/com/example/solarsports/LoginActivity.java
+++ app/src/main/java/com/example/solarsports/LoginActivity.java
@@ -6,23 +6,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     Button login;
+
+    TextView registrarse;
+
+    TextView recuperar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         login = findViewById(R.id.btnIngresar);
+        registrarse = findViewById(R.id.textViewRegistrar);
+        recuperar = findViewById(R.id.textViewRecuperar);
 
         Intent home = new Intent(this, HomeActivity.class);
+        Intent registrarView = new Intent(this, Registrar.class);
+        Intent recuperarView = new Intent(this, Recuperar.class);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 startActivity(home);
             }
         });
 
+        registrarse.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(registrarView);
+            }
+        });
+
+        recuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(recuperarView);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/solarsports/Registrar.java
+++ app/src/main/java/com/example/solarsports/Registrar.java
@@ -3,12 +3,20 @@ package com.example.solarsports;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Registrar extends AppCompatActivity {
+
+    Button btnRegistrar;
+    ImageView ImageViewBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
     }
+
+
+
 }
