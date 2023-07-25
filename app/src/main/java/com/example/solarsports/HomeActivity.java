f619d0e2c051b67b0f095c78a97dc16147eb24ce
victor mican <victormican@gmail.com>
2023-07-25 15:54:20 -0500

    Version 1.0.2 Logica Navegacion Home para Exit

--- app/src/main/java/com/example/solarsports/HomeActivity.java
+++ app/src/main/java/com/example/solarsports/HomeActivity.java
@@ -1,14 +1,29 @@
 package com.example.solarsports;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ImageView exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        exit = findViewById(R.id.imageViewExit);
+
+        Intent login = new Intent(this, LoginActivity.class);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(login);
+            }
+        });
     }
 }
