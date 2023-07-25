06c977d7bdbf58755be0b97d099e1efb9628c6c7
victor mican <victormican@gmail.com>
2023-07-25 15:27:05 -0500

    Version 1.0.1 Logica Navegacion a Home

--- app/src/main/java/com/example/solarsports/LoginActivity.java
+++ app/src/main/java/com/example/solarsports/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.solarsports;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity {
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = findViewById(R.id.btnIngresar);
+
+        Intent home = new Intent(this, HomeActivity.class);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(home);
+            }
+        });
+
     }
 }
